8b643c1ac01754a0a7e568c9b37e92aff936da9d
kheperV3 <louis@ros.fr>
2018-07-23 15:52:21 +0200

    Update action-louisros.name.py

--- action-louisros.name.py
+++ action-louisros.name.py
@@ -22,57 +22,20 @@ def CString(s):
 def intents_callback(hermes, intentMessage) : 
     
 
-    if intentMessage.intent.intent_name == 'louisros:selectStation' :
-
-        station = intentMessage.slots.radioName.first().value  
-        fv = open("/var/lib/snips/skills/live","r")
-        live = int(PyString(fv.read()))
-        fv.close()              
-        if live == 0 :
-            live = 1
-        if live == 4 :
-            live = 2        
-        fv =open("/var/lib/snips/skills/live","w") 
-        fv.write(CString(str(live)))
-        fv.close()
-
-        fv=open("/var/lib/snips/skills/link","w")
-        fv.write(CString(links[station]))
-        fv.close()
-        resul = ""
+    if intentMessage.intent.intent_name == 'louisros:name' :
+            n = ""
+            hermes.publish_continue_session(intentMessage.session_id,"premier caractère",["louisros:signe"])
+        
       
-    elif intentMessage.intent.intent_name == 'louisros:changeVolume' :
-        vol = intentMessage.slots.var.first().value 
-        fv =open("/var/lib/snips/skills/volume","r")
-        volume = PyString(fv.read())
-        fv.close()
-        if vol == "plus fort":
-            v = int(volume)
-            v = v + 1
-            if v > 10 :
-                v = 10
-            volume = str(v) 
-        elif vol == "moins fort":
-            v = int(volume)
-            v = v - 1
-            if v < 0 :
-                v = 0
-            volume =str(v)
-        else:
-            volume = str(int(vol)) 
-          
- 
-        fv =open("/var/lib/snips/skills/volume","w")
-        fv.write(CString(volume))
-        fv.close()
-        live = 3
-        fv = open("/var/lib/snips/skills/live","w") 
-        fv.write(CString(str(live)))
-        fv.close()
-        resul = ""
-            
-            
-            
+    elif intentMessage.intent.intent_name == 'louisros:signe' :
+            v = intentMessage.slots.v.first().value 
+            if v != 'fin' :
+                  n = n + v
+                  hermes.publish_continue_session(intentMessage.session_id,"suivant",["louisros:signe"])
+            else:
+                  resul = n
+                  
+              
     
     current_session_id = intentMessage.session_id
     hermes.publish_end_session(current_session_id, resul)
