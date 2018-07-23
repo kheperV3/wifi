890504cb8a84f43855164a6f23f4fe91944d03a5
kheperV3 <louis@ros.fr>
2018-07-23 17:24:32 +0200

    Update action-louisros.name.py

--- action-louisros.name.py
+++ action-louisros.name.py
@@ -20,19 +20,18 @@ def CString(s):
       return s 
 
 def intents_callback(hermes, intentMessage) : 
-    global v
-    global n
+  
 
     if intentMessage.intent.intent_name == 'louisros:name' :
             n = ""
+            v = ""
             while v != 'fin' :
                   hermes.publish_continue_session(intentMessage.session_id,"premier caractère",["louisros:signe"])
             
                         
       
     elif intentMessage.intent.intent_name == 'louisros:signe' :
-            v = intentMessage.slots.s.first().value 
-            
+            v = intentMessage.slots.s.first().value            
             if v != 'fin' :
                   n = n + v
             else:
