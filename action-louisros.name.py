ec8d91bd13afd326c3d98735242fbd44095c759e
kheperV3 <louis@ros.fr>
2018-07-24 18:27:01 +0200

    Update action-louisros.name.py

--- action-louisros.name.py
+++ action-louisros.name.py
@@ -39,9 +39,10 @@ def intents_callback(hermes, intentMessage) :
       
     elif intentMessage.intent.intent_name == 'louisros:signe' :
             v = intentMessage.slots.s.first().value 
-            hermes.publish_end_session(intentMessage.session_id, v)
+
             if v != 'fin' :
                   n = n + v
+                  hermes.publish_end_session(intentMessage.session_id, v)
                   enc = 1
                   hermes.publish_continue_session(intentMessage.session_id,"suivant",["louisros:signe","louisros:name"])
             else:
