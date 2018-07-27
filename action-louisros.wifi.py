9bdc6c4017ae6c9754b598b5c84894f2458f621c
kheperV3 <louis@ros.fr>
2018-07-27 18:35:32 +0200

    Update action-louisros.wifi.py

--- action-louisros.wifi.py
+++ action-louisros.wifi.py
@@ -23,19 +23,21 @@ def CString(s):
 def intents_callback(hermes, intentMessage) : 
   
 
-    if intentMessage.intent.intent_name == 'louisros:name' :
-            global n 
-            global nn
-            n = "" 
-            nn = ""
-
-            hermes.publish_continue_session(intentMessage.session_id,"premier caractère",["louisros:signe","louisros:name"])
+    if intentMessage.intent.intent_name == 'louisros:changeSSID' :
+            global ssid 
+            global key
+            global phase
+            ssid = "" 
+            key = ""
+            phase = 0
+            hermes.publish_continue_session(intentMessage.session_id,"nouvel SSID premier caractère",["louisros:signe"])
                                            
       
     elif intentMessage.intent.intent_name == 'louisros:signe' :
             s = ""
             m = ""
             t = ""
+            n = ""
       
             if len(intentMessage.slots.s):
                   s = intentMessage.slots.s.first().value 
@@ -49,15 +51,35 @@ def intents_callback(hermes, intentMessage) :
                   if t == 'grand' :
                         s = string.upper(s) 
                         
-                  n = n + " " + s  
-                  nn = nn + s
-                  hermes.publish_continue_session(intentMessage.session_id,"suivant",["louisros:signe","louisros:name"])
-            else:
+                  n = n + s  
                   
-                          
-                  hermes.publish_end_session(intentMessage.session_id, n)
-
-
+                  hermes.publish_continue_session(intentMessage.session_id,"suivant",["louisros:signe"])
+            else:
+                  if phase == 0 :
+                        ssid = n
+                        hermes.publish_continue_session(intentMessage.session_id,"Ce nom de SSID est-il correct? ",["louisros:ssidOK"])
+                        phase = 1
+                  else:
+                        key = n
+                        hermes.publish_continue_session(intentMessage.session_id,"Validez vous cette clé? ",["louisros:keyOK"])
+                        
+    elif intentMessage.intent.intent_name == 'louisros:ssidOK' :
+            ok = intentMessage.slots.ok.first().value 
+            if ok != "oui":
+                  hermes.publish_end_session(intentMessage.session_id, "mise à jour abandonnée")
+            else:
+                  hermes.publish_continue_session(intentMessage.session_id,"nouvelle clé",["louisros:changeKEY"])
+      
+    elif intentMessage.intent.intent_name == 'louisros:keyOK' :   
+            ok = intentMessage.slots.ok.first().value 
+            if ok != "oui":
+                  hermes.publish_end_session(intentMessage.session_id, "mise à jour abandonnée")
+            else:
+                  r = ssid + "  " + key
+                  hermes.publish_end_session(intentMessage.session_id,r)
+      
+    elif intentMessage.intent.intent_name == 'louisros:changeKEY' :
+            hermes.publish_continue_session(intentMessage.session_id,"nouvelle clé premier caractère",["louisros:signe"])
 if __name__ == "__main__":
     with Hermes(MQTT_ADDR) as h:           
         h.subscribe_intents(intents_callback).start()
